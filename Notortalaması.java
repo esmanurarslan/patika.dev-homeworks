1f79bac9d1d03452098a51c92ed841dcc4e42b5f
esmanurarslan <96125581+esmanurarslan@users.noreply.github.com>
2022-05-15 15:34:19 +0300

    Add files via upload

---
+++ Notortalaması.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class Notortalamasý {
+	public static void main(String[] args) {
+		int mat,fizik,kimya,turkce,tarih,muzik;
+		Scanner input= new Scanner(System.in);
+		
+		System.out.print("matematik notunu giriniz:");
+		mat = input.nextInt();
+		
+		System.out.print("fizik notunu giriniz:");
+		fizik = input.nextInt();
+		
+		System.out.print("kimya notunu giriniz:");
+		kimya = input.nextInt();
+		
+		System.out.print("türkçe notunu giriniz:");
+		turkce = input.nextInt();
+		
+		System.out.print("tarih notunu giriniz:");
+		tarih = input.nextInt();
+		
+		System.out.print("müzik notunu giriniz:");
+		muzik = input.nextInt();
+		
+		int toplam= (mat+fizik+kimya+turkce+tarih+muzik);
+		double ortalama=(toplam/6.00);
+		System.out.println("ortalama notunuz:"+ortalama);
+		
+		boolean sonuc=(ortalama<60);
+		String str=(sonuc)? "Sýnýfta Kaldý":"Sýnýfý Geçti";
+		System.out.println(str);
+		
+	}
+	
+	
+
+}
